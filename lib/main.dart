2b7f1099756616a7540a239136a5d2fe1d6cb315
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-09 00:36:18 +0530

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:simple_api_app/Repository/landing_page.dart';
+import 'package:simple_api_app/view/landing_page.dart';
 import 'package:simple_api_app/view/home.dart';
 
 
